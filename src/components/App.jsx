49c86e745afaf8e2f0321878ff93ffdd766b719d
Himanshu Rajput <himanshurajput.career@gmail.com>
2020-09-11 22:00:29 +0530

    Setting Up for challenge 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Note from './Note';
-import notes from '../notes';
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
-    return <div>
-    <Header />
-    {notes.map( note => <Note 
-        key= {note.key}
-        title= {note.title}
-        content= {note.content}
-    />)}
-    <Footer />
+  return (
+    <div>
+      <Header />
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
+      <Footer />
     </div>
+  );
 }
 
 export default App;
