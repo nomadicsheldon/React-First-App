5d1fcc1ab190cd031d0c7f5becc074435823e1c5
Himanshu Rajput <himanshurajput.career@gmail.com>
2020-09-12 11:56:16 +0530

    complete functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    const newItem = {
+      title: note.title,
+      content: note.content,
+    };
+    setNotes((prevNote) => {
+      return [...prevNote, newItem];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((item, idx) => {
+        return idx !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, idx) => {
+        return (
+          <Note
+            key={idx}
+            id={idx}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
